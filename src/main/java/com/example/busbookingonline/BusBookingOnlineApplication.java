7cbe76f22c0622e3fba4ce52840105bb1957a504
nd199 <narend251999@gmail.com>
2022-06-20 21:30:32 +0530

    Initial commit

---
+++ src/main/java/com/example/busbookingonline/BusBookingOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.busbookingonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBookingOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusBookingOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/busbookingonline/BusBookingOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busbookingonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusBookingOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
